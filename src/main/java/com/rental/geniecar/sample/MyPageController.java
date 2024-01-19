ed0f3dcf74018fa6a0661e0842da63ef1edf51e9
User -2023TWLRF\Administrator <aaa@aaa.aaa>
2024-01-19 09:40:39 +0900

    마이페이지 패키지 삭제

--- src/main/java/com/rental/geniecar/sample/MyPageController.java
+++ src/main/java/com/rental/geniecar/sample/MyPageController.java
@@ -1,4 +1,4 @@
-package com.rental.geniecar.mypage.controller;
+package com.rental.geniecar.sample;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
