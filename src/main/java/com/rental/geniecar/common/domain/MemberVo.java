d59c5c298f5003e485c6af2082dd3dbad6c469e1
User -2023TWLRF\Administrator <aaa@aaa.aaa>
2024-01-24 17:55:27 +0900

    vo 생성중

---
+++ src/main/java/com/rental/geniecar/common/domain/MemberVo.java
@@ -0,0 +1,23 @@
+package com.rental.geniecar.common.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Date;
+
+@Getter
+@Setter
+public class MemberVo {
+    private String id;                    // 아이디
+    private String pw;                    // 비밀번호
+    private String name;                  // 이름
+    private String gender;                // 성별 (M: 남 F: 여)
+    private Date birthday;                // 생년월일 ('YYYY-MM-DD')
+    private String hp;                    // 휴대폰 (FORMAT: XXX-XXXX-XXXX)
+    private String address;               // 주소
+    private String addressDetail;         // 상세주소
+    private String zipCode;               // 우편번호
+    private Date joinDate;                // 가입일
+    private Date lastLoginDate;           // 마지막 로그인 일시
+    private String withdrawalYn;          // 탈퇴 여부
+    private String type;                  // 회원 타입 (U: 사용자 B: 지점 A: 사이트 관리자)
+}
---
+++ src/main/java/com/rental/geniecar/common/domain/RentalCarVo.java
@@ -0,0 +1,17 @@
+package com.rental.geniecar.common.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class RentalCarVo {
+    private String carIdentificationNumber;   // 차대번호
+    private String carTypeCode;               // 차종코드 (공통코드 FK) 소형 준중형 중형 준대형 대형 ...
+    private String carNameCode;               // 차량명코드 (공통코드 FK) 소나타, 스파크, k9 ..
+    private String carFuelCode;               // 연료코드 (공통코드 FK) 디젤, 휘발유, 전기 ..
+    private Date deliveryDate;                // 출고일시
+    private String company;                   // 제조사 (공통코드 FK) 현대, 기아 ...
+}
